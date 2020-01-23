fa0d21d290da62e80ec5620913167b764b0d0048
Felipe <felipy_erivaldo@hotmail.com>
2020-01-23 10:24:41 -0300

    adicionado codigo que convert array em collection

--- convert_array_of_array_in_collection.php
+++ convert_array_of_array_in_collection.php
@@ -2,3 +2,5 @@
 $response['comissionados']  = collect($this->apiGetAllComissoesFuncionarios())->map(function ($comissionado) {
     return (object) $comissionado;
 });
+
+//fonte : https://stackoverflow.com/questions/54042847/laravel-convert-array-into-eloquent-collection
